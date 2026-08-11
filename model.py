15ad14c26911c4ca33cfab7019fa1528fd874f57
rahul <rahulsharma@gmail.com>
2026-08-12 00:54:56 +0530

    model add to the project

---
+++ model.py
@@ -0,0 +1,10 @@
+# train.py
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+
+X, y = load_iris(return_X_y=True)
+
+model = RandomForestClassifier(random_state=42)
+model.fit(X, y)
+
+print("Model trained successfully!")
